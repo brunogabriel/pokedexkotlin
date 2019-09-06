aa580ad23031417ab047a7a6cd5cb3da338da021
Bruno Santos <brunogsantoswork@gmail.com>
2019-09-06 08:44:34 -0300

    #COMMENT * Changed test library to use androidx in Junit; * Added test to find pokemon by id in PokemonService

--- PokeDexKotlin/app/src/androidTest/java/io/github/brunogabriel/pokedexkotlin/networking/PokemonServiceTest.kt
+++ PokeDexKotlin/app/src/androidTest/java/io/github/brunogabriel/pokedexkotlin/networking/PokemonServiceTest.kt
@@ -1,8 +1,11 @@
 package io.github.brunogabriel.pokedexkotlin.networking
 
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit.WireMockRule
+import io.github.brunogabriel.pokedexkotlin.shared.model.Pokemon
+import io.github.brunogabriel.pokedexkotlin.shared.model.PokemonServiceResponse
+import io.github.brunogabriel.pokedexkotlin.shared.model.PokemonSprites
 import io.github.brunogabriel.pokedexkotlin.shared.networking.PokemonService
 import io.github.brunogabriel.pokedexkotlin.shared.networking.RetrofitManager
 import org.junit.Before
@@ -13,6 +16,7 @@ import org.junit.runner.RunWith
 /**
  * Created by brunosantos on 2019-09-05.
  */
+@Suppress("MemberVisibilityCanBePrivate")
 @RunWith(AndroidJUnit4::class)
 class PokemonServiceTest {
     val mockServer = WireMockRule()
@@ -29,24 +33,58 @@ class PokemonServiceTest {
     @Test
     fun shouldFindPokemonList() {
         // given
+        val expected = PokemonServiceResponse(1, "anyNext",
+            listOf(Pokemon(name = "charmander")))
         mockServer.stubFor(get(urlPathEqualTo("/pokemon"))
             .withQueryParam("limit", equalTo("251"))
             .willReturn(aResponse()
                 .withStatus(200)
                 .withBody("{" +
-                        "    \"count\": 1," +
-                        "    \"next\": \"anyNext\"," +
-                        "    \"results\": [" +
-                        "        {" +
-                        "            \"name\": \"charmander\"" +
-                        "        }" +
-                        "    ]" +
+                        "\"count\": 1," +
+                        "\"next\": \"anyNext\"," +
+                        "\"results\": [" +
+                        "   {" +
+                        "       \"name\": \"charmander\"" +
+                        "   }" +
+                        "]" +
                         "}")
             ))
 
         // then
-        pokemonService.findPokemons().test().assertComplete().assertValue {
-            it.isSuccessful && it.body()!!.count == 1 && it.body()!!.results[0].name == "charmander"
-        }.dispose()
+        pokemonService.findPokemons().test()
+            .assertComplete()
+            .assertValue { it.isSuccessful && it.body() == expected }
+            .dispose()
+    }
+
+    @Test
+    fun shouldFindPokemonById() {
+        // given
+        val expected = Pokemon(1L, "bulbasaur", "anyUrl", 100,
+            PokemonSprites("back_default", "back_shiny",
+                "front_default", "front_shiny")
+        )
+        mockServer.stubFor(get(urlPathEqualTo("/pokemon/1"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withBody("{" +
+                        "    \"number\": 1," +
+                        "    \"name\": \"bulbasaur\"," +
+                        "    \"url\": \"anyUrl\"," +
+                        "    \"height\": 100," +
+                        "    \"sprites\": {" +
+                        "        \"back_default\": \"back_default\"," +
+                        "        \"back_shiny\": \"back_shiny\"," +
+                        "        \"front_default\": \"front_default\"," +
+                        "        \"front_shiny\": \"front_shiny\"" +
+                        "    }" +
+                        "}")
+            ))
+
+        // then
+        pokemonService.findPokemonById(1L).test()
+            .assertComplete()
+            .assertValue { it.isSuccessful && it.body() == expected }
+            .dispose()
     }
 }
