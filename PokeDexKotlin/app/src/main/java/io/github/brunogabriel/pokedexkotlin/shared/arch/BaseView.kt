cf1a0dac1fa93b703818f838745aaa6b15999833
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2019-10-10 22:29:00 -0300

    Reestructuring this project to using koin

--- PokeDexKotlin/app/src/main/java/io/github/brunogabriel/pokedexkotlin/shared/arch/BaseView.kt
+++
@@ -1,8 +0,0 @@
-package io.github.brunogabriel.pokedexkotlin.shared.arch
-
-/**
- * Created by brunogabriel on 2019-08-29.
- */
-interface BaseView<T> {
-    var presenter: T
-}
--- PokeDexKotlin/app/src/main/java/io/github/brunogabriel/pokedexkotlin/shared/extensions/PrimitiveExtensions.kt
+++
@@ -1,8 +0,0 @@
-package io.github.brunogabriel.pokedexkotlin.shared.extensions
-
-import android.content.res.Resources
-
-/**
- * Created by brunogabriel on 2019-08-29.
- */
-fun Int.toDP() = (this * Resources.getSystem().displayMetrics.density).toInt()
