1930664a5d329d368163e19cc6bd896848b3d238
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2020-02-26 09:17:20 -0300

    Added initial modules

--- PokeDexKotlin/buildSrc/src/main/java/dependencies/Modules.kt
+++ PokeDexKotlin/buildSrc/src/main/java/dependencies/Modules.kt
@@ -4,4 +4,8 @@ package dependencies
  * Created by bruno on 2020-02-26
  */
 object Modules {
+    // defaults
+    const val STYLEGUIDE = ":styleGuide"
+    const val CORE = ":core"
+    const val CORETEST = ":coreTest"
 }
