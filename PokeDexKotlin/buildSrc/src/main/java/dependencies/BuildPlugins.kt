15f98d0ac0af08076dd5f98078d11a69e74fd727
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2020-02-26 10:25:04 -0300

    Added lints detectors

--- PokeDexKotlin/buildSrc/src/main/java/dependencies/BuildPlugins.kt
+++ PokeDexKotlin/buildSrc/src/main/java/dependencies/BuildPlugins.kt
@@ -7,4 +7,6 @@ object BuildPlugins {
     const val GRADLE = "com.android.tools.build:gradle:${PluginsVersions.GRADLE}"
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginsVersions.KOTLIN}"
     const val JACOCO = "org.jacoco:org.jacoco.core:${PluginsVersions.JACOCO}"
+    const val KTLINT = "com.pinterest:ktlint:${PluginsVersions.KTLINT}"
+    const val DETEKT = "io.gitlab.arturbosch.detekt:detekt-cli:${PluginsVersions.DETEKT}"
 }
--- PokeDexKotlin/buildSrc/src/main/java/dependencies/PluginsVersions.kt
+++ PokeDexKotlin/buildSrc/src/main/java/dependencies/PluginsVersions.kt
@@ -7,4 +7,6 @@ object PluginsVersions {
     const val KOTLIN = "1.3.61"
     const val GRADLE = "3.5.3"
     const val JACOCO = "0.8.2"
+    const val KTLINT = "0.36.0"
+    const val DETEKT = "1.2.2"
 }
