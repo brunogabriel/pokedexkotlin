6c8d3fe46ba4a9f8cee7b29004acee3d5734d787
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2020-02-27 09:21:25 -0300

    Added core package

--- PokeDexKotlin/buildSrc/src/main/java/dependencies/Modules.kt
+++ PokeDexKotlin/buildSrc/src/main/java/dependencies/Modules.kt
@@ -4,6 +4,9 @@ package dependencies
  * Created by bruno on 27/02/20
  */
 object Modules {
-
+    object CORE {
+        const val STYLEGUIDE = ":core:style-guide"
+        const val TEST = ":core:core-test"
+    }
 }
 
