670ff075be782450dd2b88045b23f92ce20f6f04
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2020-02-27 10:07:25 -0300

    lint fixes

--- PokeDexKotlin/features/pokemon-list/src/main/java/io/github/brunogabriel/pokemon_list/presentation/PokemonMainActivity.kt
+++ PokeDexKotlin/features/pokemon-list/src/main/java/io/github/brunogabriel/pokemon_list/presentation/PokemonMainActivity.kt
@@ -5,5 +5,4 @@ import androidx.appcompat.app.AppCompatActivity
 /**
  * Created by bruno on 27/02/20
  */
-class PokemonMainActivity : AppCompatActivity() {
-}
+class PokemonMainActivity : AppCompatActivity()
