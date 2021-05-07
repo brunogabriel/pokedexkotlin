a0ca93049adb8d3f46a1cfcff8a1f5f1513bdebd
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2021-05-07 10:00:09 -0300

    new-develop-kts #COMMENT * Fixed build and target sdk

--- AndroidPokeDex/buildSrc/src/main/kotlin/Config.kt
+++ AndroidPokeDex/buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ object Config {
     const val applicationId = "io.github.brunogabriel.androidpokedex"
     const val compileSdk = 30
     const val minSdk = 21
-    const val targetSdk = 21
+    const val targetSdk = 30
     const val versionCode = 1
     const val versionName = "1.0.0"
     const val buildToolsVersion = "29.0.3"
